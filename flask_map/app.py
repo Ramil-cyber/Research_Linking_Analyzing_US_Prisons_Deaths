ee8cc7c27130ef1a29d36679a75e1285aacb283e
Ramil Mammadov <ramil.mammadov2021@gmail.com>
2025-06-30 19:53:07 -0400

    updated dataset

--- flask_map/app.py
+++ flask_map/app.py
@@ -5,23 +5,26 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     # Sample Plotly graph (a simple line chart)
     data = [
         go.Scatter(
             x=[1, 2, 3, 4, 5],
             y=[10, 14, 18, 24, 30],
-            mode='lines+markers',
-            name='Line Plot'
+            mode="lines+markers",
+            name="Line Plot",
         )
     ]
 
-    layout = go.Layout(title='Simple Plotly Line Chart')
-    graphJSON = json.dumps({'data': data, 'layout': layout}, cls=plotly.utils.PlotlyJSONEncoder)
+    layout = go.Layout(title="Simple Plotly Line Chart")
+    graphJSON = json.dumps(
+        {"data": data, "layout": layout}, cls=plotly.utils.PlotlyJSONEncoder
+    )
 
     return render_template("index.html", graphJSON=graphJSON)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
